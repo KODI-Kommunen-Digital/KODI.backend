6552aa2ad951be29302c989cc7c8d5a7ea22bb28
Ajay Umakanth <ajay@heidi-app.de>
2023-02-02 16:01:23 +0100

    Add the following endpoints - cities - categories - roles - sources

--- constants/tableNames.js
+++ constants/tableNames.js
@@ -1,4 +1,9 @@
 module.exports = {
   USER_TABLE: 'users',
-  ROLES_TABLE: 'roles'
+  ROLES_TABLE: 'roles',
+  CITIES_TABLE: 'cities',
+  CATEGORIES_TABLE: 'categories',
+  ROLESS_TABLE: 'roles',
+  STAUSES_TABLE: 'statuses',
+  SOURCES_TABLE: 'sources'
 }
---
+++ routes/categories.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const database = require('../services/database');
+const tables = require('../constants/tableNames');
+
+router.get('/', async function(req, res, next) {
+    database.get(tables.CATEGORIES_TABLE).then((response) => {
+        let data = response.rows;
+        res.status(200).json({
+            status: "success",
+            data: data,
+        });
+    }).catch((err) => {
+        return next(new AppError(err));
+    });
+});
+
+module.exports = router;
