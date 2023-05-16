44472711f67576f1332fb8e1361e1f90c9d2f763
SoSa-HEIDI <sonu@heidi-app.de>
2023-05-16 08:35:30 +0200

    email text update

--- emailTemplates/en/verificationDone.js
+++ emailTemplates/en/verificationDone.js
@@ -3,7 +3,7 @@ module.exports = function (firstName, lastName) {
         subject: "Your email has been verified",
         body: `<h1>Your email has been verified</h1>
                 <p>Dear ${firstName} ${lastName},<br>
-                You have reset your succesfully verified your account. You can now login to your account<br>
+                You have reset your succesfully verified your account. You can now login to your account.<br>
                 <br>
                 Thank you,<br>
                 Heidi Team</p>`
