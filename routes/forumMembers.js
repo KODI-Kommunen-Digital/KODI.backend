65f4591535f53ad80b8814562b001996593b4efd
Ajay Umakanth <ajay@heidi-app.de>
2023-06-20 16:21:29 +0200

    Add delete member

--- routes/forumMembers.js
+++ routes/forumMembers.js
@@ -30,10 +30,12 @@ router.get("/", authentication, async function (req, res, next) {
         return next(new AppError(`Invalid forumId`, 400));
     } else {
         try {
-            const response = await database.get(tables.FORUM_MEMBERS, { forumId }, null, cityId);
+            let response = await database.get(tables.FORUMS, { id: forumId }, null, cityId);
             if (response.rows && response.rows.length === 0) {
                 return next(new AppError(`Forum not present '${forumId}' given`, 404));
             }
+
+            response = await database.get(tables.FORUM_MEMBERS, { forumId }, null, cityId);
 
             const memberCityUserIds = await database.get(tables.USER_CITYUSER_MAPPING_TABLE, {
                 cityUserId: response.rows.map(x => {
@@ -56,10 +58,67 @@ router.get("/", authentication, async function (req, res, next) {
                     "phoneNumber"
                 ]
             })
+
+            if (!memberUserIds.find(u => u.id === req.userId)) {
+                return next(new AppError(`You are not allowed to access this resource`, 403));
+            }
         
             res.status(200).json({
                 status: "success",
                 data: memberUserIds.rows,
+            });
+        } catch (err) {
+            return next(new AppError(err));
+        }
+    }
+	
+});
+
+router.delete("/:id", authentication, async function (req, res, next) {
+    const forumId = req.forumId
+    const cityId = req.cityId;
+    const userId = req.userId;
+    const memberId = req.params.id;
+
+    if (isNaN(Number(cityId)) || Number(cityId) <= 0) {
+        return next(new AppError(`Invalid cityId`, 400));
+    } else {
+        try {
+            const response = await database.get(tables.CITIES_TABLE, { id: cityId });
+            if (response.rows && response.rows.length === 0) {
+                return next(new AppError(`City not present '${cityId}' given`, 404));
+            }
+            if (!response.rows[0].hasForums) {
+                next(new AppError(`CityId ${cityId} can not create forum related endpoints`, 403));
+            }
+        } catch (err) {
+            return next(new AppError(err));
+        }	
+    }
+
+    if (isNaN(Number(forumId)) || Number(forumId) <= 0) {
+        return next(new AppError(`Invalid forumId`, 400));
+    } else {
+        try {
+            let response = await database.get(tables.USER_CITYUSER_MAPPING_TABLE, { userId, cityId }, null, cityId);
+            if (response.rows && response.rows.length === 0) {
+                return next(new AppError(`Forum not present '${forumId}' given`, 404));
+            }
+            const cityUserId = response[0].cityUserId;
+
+            response = await database.get(tables.FORUMS, { id: forumId }, null, cityId);
+            if (response.rows && response.rows.length === 0) {
+                return next(new AppError(`Forum not present '${forumId}' given`, 404));
+            }
+
+            response = await database.get(tables.FORUM_MEMBERS, { forumId, userId: cityUserId }, null, cityId);
+            if (response.id !== memberId || !response.isAdmin) {
+                return next(new AppError(`You are not allowed to call this endpoint`, 403));
+            }
+            await database.deleteData(tables.FORUM_MEMBERS, { forumId, userId: cityUserId }, cityId);
+
+            res.status(200).json({
+                status: "success"
             });
         } catch (err) {
             return next(new AppError(err));
