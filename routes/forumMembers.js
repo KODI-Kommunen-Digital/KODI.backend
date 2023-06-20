5a25ebeb935cf8b4f71ff4dafa2bd6d820470ea2
Ajay Umakanth <ajay@heidi-app.de>
2023-06-20 16:31:38 +0200

    Add patch endpoint for members

--- routes/forumMembers.js
+++ routes/forumMembers.js
@@ -127,4 +127,67 @@ router.delete("/:id", authentication, async function (req, res, next) {
 	
 });
 
+
+router.patch("/:id", authentication, async function (req, res, next) {
+    const forumId = req.forumId
+    const cityId = req.cityId;
+    const userId = req.userId;
+    const memberId = req.params.id;
+    const payload = req.body;
+
+    if (isNaN(Number(cityId)) || Number(cityId) <= 0) {
+        return next(new AppError(`Invalid cityId`, 400));
+    } else {
+        try {
+            const response = await database.get(tables.CITIES_TABLE, { id: cityId });
+            if (response.rows && response.rows.length === 0) {
+                return next(new AppError(`City not present '${cityId}' given`, 404));
+            }
+            if (!response.rows[0].hasForums) {
+                next(new AppError(`CityId ${cityId} can not create forum related endpoints`, 403));
+            }
+        } catch (err) {
+            return next(new AppError(err));
+        }	
+    }
+
+    if (isNaN(Number(forumId)) || Number(forumId) <= 0) {
+        return next(new AppError(`Invalid forumId`, 400));
+    } else {
+        try {
+            let response = await database.get(tables.USER_CITYUSER_MAPPING_TABLE, { userId, cityId }, null, cityId);
+            if (response.rows && response.rows.length === 0) {
+                return next(new AppError(`Forum not present '${forumId}' given`, 404));
+            }
+            const cityUserId = response[0].cityUserId;
+
+            response = await database.get(tables.FORUMS, { id: forumId }, null, cityId);
+            if (response.rows && response.rows.length === 0) {
+                return next(new AppError(`Forum not present '${forumId}' given`, 404));
+            }
+
+            response = await database.get(tables.FORUM_MEMBERS, { forumId, userId: cityUserId }, null, cityId);
+            const updationData = {};
+
+            if (payload.isAdmin) {
+                if (!response.isAdmin) {
+                    return next(new AppError(`You are not allowed to call this endpoint`, 403));
+                } else {
+                    updationData.isAdmin = response.isAdmin;
+                }
+            }
+
+            if (updationData) {
+                await database.update(tables.FORUM_MEMBERS, updationData, { id: memberId }, cityId);
+            }
+
+            res.status(200).json({
+                status: "success"
+            });
+        } catch (err) {
+            return next(new AppError(err));
+        }
+    }
+	
+});
 module.exports = router;
