32dc47f1ec0821d7ab4fc2da18fd3c588bdc4ff5
SoSa-HEIDI <sonu@heidi-app.de>
2023-07-27 11:29:37 +0200

    email issues fixed for password reset

--- emailTemplates/de/resetPasswordEmail.js
+++ emailTemplates/de/resetPasswordEmail.js
@@ -3,7 +3,7 @@ module.exports = function (firstName, lastName, token, userId) {
         subject: "Ihr Passwort wurde zurückgesetzt",
         body: `<h1>Ihr Passwort wurde zurückgesetzt</h1>
                 <p>Liebes  ${firstName} ${lastName},
-                das Passwort für dein Konto wurde erfolgreich zurückgesetzt.<br>
+                für dein Konto wurde eine Passwortänderung beantragt. Wenn dies auf dich zutrifft, verwende bitte den unten stehen den Link, um dein Passwort zurückzusetzen.<br>
                 <a href="${process.env.WEBSITE_DOMAIN}/PasswordForgot?token=${token}&userId=${userId}">Passwort vergessen</a>
                 <br>
                 Mit freundlichen Gruessen,<br>
