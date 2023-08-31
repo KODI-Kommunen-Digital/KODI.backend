249edec88779b3d94c22d3dee2984773c9cd3519
SoSa-HEIDI <sonu@heidi-app.de>
2023-08-31 10:28:11 +0530

    email translations

--- emailTemplates/de/passwordResetDone.js
+++ emailTemplates/de/passwordResetDone.js
@@ -1,11 +1,12 @@
 module.exports = function (firstName, lastName) {
     return {
-        subject: "Ihr Passwort wurde zurückgesetzt",
-        body: `<h1>Ihr Passwort wurde zurückgesetzt</h1>
-                <p>Liebes ${firstName} ${lastName},<br>
-                Sie haben Ihr Passwort für Ihr Konto erfolgreich zurückgesetzt.<br>
+        subject: "Dein Passwort wurde zurückgesetzt",
+        body: `<h1>Dein Passwort wurde zurückgesetzt</h1>
+                <p>Hey ${firstName} ${lastName},<br>
+                Dein Passwort wurde erfolgreich zurückgesetzt.<br>
                 <br>
-                Mit freundlichen Gruessen,<br>
-                Heidi Team</p>`
+                Liebe Grüße,<br>
+                Das ${process.env.REGION}-Team</p>`
+
     }
 }
