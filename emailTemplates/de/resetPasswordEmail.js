4a376fcb69cea2b618a81e5147825354fe2a8c58
AjUm-HEIDI <ajay@heidi-app.de>
2023-09-26 11:17:13 +0200

    Update german translations

--- emailTemplates/de/resetPasswordEmail.js
+++ emailTemplates/de/resetPasswordEmail.js
@@ -3,7 +3,7 @@ module.exports = function (firstName, lastName, token, userId) {
         subject: "Setze dein Passwort zurück",
         body: `<h1> Setze dein Passwort zurück</h1>
                 <p>Hey ${firstName} ${lastName},<br>
-                für dein Konto wurde eine Passwortänderung beantragt. Wenn dies auf dich zutrifft, verwende bitte den unten stehen den Link, um dein Passwort zurückzusetzen.<br>
+                für dein Konto wurde eine Passwortänderung beantragt. Wenn dies auf dich zutrifft, verwende bitte den unten stehenden Link, um dein Passwort zurückzusetzen.<br>
                 <a href="${process.env.WEBSITE_DOMAIN}/PasswordForgot?token=${token}&userId=${userId}">Passwort vergessen</a>
                 <br>
                 Liebe Grüße!,<br>
