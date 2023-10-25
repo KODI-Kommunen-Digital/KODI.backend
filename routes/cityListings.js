5b4e4d67febeb1ad5e0d3d9221c6193156712e33
AjUm-HEIDI <ajay@heidi-app.de>
2023-10-25 14:28:48 +0200

    Refactor delete listing endpoint

--- routes/cityListings.js
+++ routes/cityListings.js
@@ -796,35 +796,39 @@ router.delete("/:id", authentication, async function (req, res, next) {
         next(new AppError(`Invalid entry ${id}`, 404));
         return;
     }
+    
+    try {
+        const response = await database.get(tables.CITIES_TABLE, {
+            id: cityId,
+        });
+        if (response.rows && response.rows.length === 0) {
+            return next(
+                new AppError(`Invalid City '${cityId}' given`, 404)
+            );
+        }
+    } catch (err) {
+        return next(new AppError(err));
+    }
 
-    let response = await database.get(
+    let response = await database.get(tables.LISTINGS_TABLE, { id }, null, cityId);
+    if (!response.rows || response.rows.length === 0) {
+        return next(new AppError(`Listing with id ${id} does not exist`, 404));
+    }
+
+    const currentListingData = response.rows[0];
+
+    response = await database.get(
         tables.USER_CITYUSER_MAPPING_TABLE,
         { userId: req.userId, cityId },
         "cityUserId"
     );
 
-    if ((!response.rows || response.rows.length === 0) && req.roleId !== roles.Admin) {
+    if (req.roleId !== roles.Admin && (!response.rows || response.rows.length === 0 || response.rows[0].cityUserId !== currentListingData.userId)) {
         return next(
             new AppError(`You are not allowed to access this resource`, 403)
         );
     }
-
-    const cityUserId = response.rows[0].cityUserId;
-
-    response = await database.get(tables.LISTINGS_TABLE, { id }, null, cityId);
-    if (!response.rows || response.rows.length === 0) {
-        return next(new AppError(`Listing with id ${id} does not exist`, 404));
-    }
-    const currentListingData = response.rows[0];
-
-    if (
-        currentListingData.userId !== cityUserId &&
-        req.roleId !== roles.Admin
-    ) {
-        return next(
-            new AppError(`You are not allowed to access this resource`, 403)
-        );
-    }
+    
     const onSucccess = async () => {
         database.deleteData(tables.LISTINGS_TABLE, { id }, cityId);
         return res.status(200).json({
