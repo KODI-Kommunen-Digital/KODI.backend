bc7dddc157d8bb1a9e278b47773ca66bce0dbfa8
SoSa-HEIDI <sonu@heidi-app.de>
2023-11-20 14:33:19 +0100

    added new subcategory timeless News

---
+++ constants/subcategories.js
@@ -0,0 +1,11 @@
+module.exports = {
+    newsflash: 1, // Eilmeldungen
+    roadTraffic: 9, // Verkehrsmeldungen
+    politics: 3, // Politik
+    ecocomy: 4, // Wirtschaft
+    sports: 5, // Sport
+    local: 7, // Lokales
+    clubnews: 8, // Vereinsnews
+    officialnotification: 10, // Amtliche Mitteilung
+    timelessNews: 11 // Zeitlose News
+};
