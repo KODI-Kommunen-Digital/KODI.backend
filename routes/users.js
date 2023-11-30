961f78d5b30a8f1538bc4d62f84f5db27d53f279
Aneesh Jose <aneesh@heidi-app.de>
2023-11-30 10:16:32 +0530

    docs: swagger docs for sendVerificationEmail endpoint

--- routes/users.js
+++ routes/users.js
@@ -839,9 +839,74 @@ router.post("/forgotPassword", forgotPassword);
  *                  message:
  *                    type: string
  *                    example: error name              
- */ 
+ */
 router.post("/resetPassword", resetPassword);
 
+/**
+ * @swagger
+ * paths:
+ *  /users/sendVerificationEmail:
+ *    post:
+ *      summary: API to send verification email
+ *      description: Send verification email to the user
+ *      tags: [send-verification-email]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                email:
+ *                  type: string
+ *                  required: true
+ *                  description: The token will be sent to this email
+ *                  example: email@example.com
+ *                language:
+ *                  type: string
+ *                  required: true
+ *                  description: The language of the email
+ *                  example: en
+ *                  enum: [en, de]
+ *                  default: de
+ *      responses:
+ *        '200':
+ *          description: The verification email was successfully sent
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  status:
+ *                    type: string
+ *                    example: success
+ *        '400':
+ *          description: Invalid input given
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  status:
+ *                    type: string
+ *                    example: error
+ *                  message:
+ *                    type: string
+ *                    example: Email not present
+ *        '500':
+ *          description: Server error
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  status:
+ *                    type: string
+ *                    example: error
+ *                  message:
+ *                    type: string
+ *                    example: error name              
+ */
 router.post("/sendVerificationEmail", async function (req, res, next) {
     const email = req.body.email;
     const language = req.body.language || "de";
