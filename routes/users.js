f173c77760cdf603387ea329dd7e28ed8e3205e3
Aneesh Jose <aneesh@heidi-app.de>
2023-11-30 19:47:13 +0530

    docs: swagger docs for delete loginDevices endpoint

--- routes/users.js
+++ routes/users.js
@@ -978,8 +978,6 @@ router.post("/sendVerificationEmail", sendVerificationEmail);
  */
 router.post("/verifyEmail", verifyEmail);
 
-
-
 /**
  * @swagger
  * paths:
@@ -1229,6 +1227,69 @@ router.get("/", getUsers);
  */
 router.post("/:id/loginDevices", authentication, listLoginDevices);
 
+/**
+ * @swagger
+ * paths:
+ *  /users/{id}/loginDevices:
+ *   delete:
+ *     summary: API to delete login devices
+ *     description: Delete login devices of a user. If the query param 'id' is not given, all the login devices of the user will be deleted.
+ *     tags: [delete-login-devices]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user
+ *         type: integer
+ *         example: 1
+ *       - in: query
+ *         name: id
+ *         schema:
+ *           type: integer
+ *           example: 1
+ *           description: The id of the login device. If not given, all the login devices of the user will be deleted
+ *           required: false
+ *     responses:
+ *        '200':
+ *          description: The login devices were successfully deleted
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  status:
+ *                    type: string
+ *                    example: success
+ *        '401':
+ *          description: Unauthorized! Invalid access token
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  status:
+ *                    type: string
+ *                    example: error
+ *                  message:
+ *                    type: string
+ *                    example: Authorization token not present
+ *        '500':
+ *          description: Server error
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  status:
+ *                    type: string
+ *                    example: error
+ *                  message:
+ *                    type: string
+ *                    example: JsonWebTokenError invalid signature
+ *          
+ */
 router.delete(
     "/:id/loginDevices",
     authentication,
