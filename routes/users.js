164267f8746b583e2967c06e1a4c060dd68478fb
Aneesh Jose <aneesh@heidi-app.de>
2023-11-30 21:15:17 +0530

    doc: swagger doc for image upload

--- routes/users.js
+++ routes/users.js
@@ -419,6 +419,75 @@ router.delete(
     }
 );
 
+/**
+ * @swagger
+ * paths:
+ *  /users/{id}/imageUpload:
+ *    post:
+ *      summary: Upload user profile image
+ *      description: Uploads a user profile image.
+ *      tags: [user-image-upload]
+ *      security:
+ *        - bearerAuth: []
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          description: The ID of the user
+ *          schema:
+ *            type: integer
+ *            example: 1
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          multipart/form-data:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                image:
+ *                  description: The image to be uploaded
+ *                  type: string
+ *                  format: binary
+ *              required:
+ *                - image
+ *      responses:
+ *        '200':
+ *          description: Image uploaded successfully
+ *          content:
+ *            application/json:
+ *              example:
+ *                status: success
+ *                data:
+ *                  image: 'user_1/profilePic'
+ *        '400':
+ *          description: Bad request, image not uploaded
+ *          content:
+ *            application/json:
+ *              example:
+ *                status: error
+ *                message: Image not uploaded
+ *        '401':
+ *          description: Unauthorized, invalid access token
+ *          content:
+ *            application/json:
+ *              example:
+ *                status: error
+ *                message: Authorization token not present
+ *        '403':
+ *          description: Forbidden, user not allowed to access resource
+ *          content:
+ *            application/json:
+ *              example:
+ *                status: error
+ *                message: You are not allowed to access this resource
+ *        '500':
+ *          description: Internal Server Error
+ *          content:
+ *            application/json:
+ *              example:
+ *                status: error
+ *                message: Failed!! Please try again
+ */
 router.post(
     "/:id/imageUpload",
     authentication,
