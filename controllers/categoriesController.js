c23a28c0da8add32b31aac60a8f114e8d19dc565
Aneesh Jose <aneesh@heidi-app.de>
2023-12-13 17:42:36 +0530

    refactor: user controllers, services for getCategoriesListingCount API

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,4 +1,5 @@
-const { getCategories } = require("../services/categoryService");
+const { getCategories, getCategoryListingCountForCity, getCategoryListingCount } = require("../services/categoryService");
+const { getCities } = require("../services/cities");
 const AppError = require("../utils/appError");
 
 const getAllCategories = async function (req, res, next) {
@@ -13,6 +14,39 @@ const getAllCategories = async function (req, res, next) {
     }
 }
 
+const getListingCount = async function (req, res, next) {
+    const params = req.query;
+
+    if (params.cityId) {
+        try {
+            const response = await getCities({ id: params.cityId });
+            if (response.length === 0) {
+                return next(
+                    new AppError(`Invalid City '${params.cityId}' given`, 404)
+                );
+            }
+        } catch (err) {
+            return next(new AppError(err));
+        }
+        const response = await getCategoryListingCountForCity(params.cityId);
+        res.status(200).json({
+            status: "success",
+            data: response
+        });
+    } else {
+        try {
+            const response = await getCategoryListingCount();
+            res.status(200).json({
+                status: "success",
+                data: response
+            });
+        } catch (err) {
+            return next(new AppError(err));
+        }
+    }
+}
+
 module.exports = {
     getAllCategories,
+    getListingCount,
 }
--- services/categoryService.js
+++ services/categoryService.js
@@ -9,6 +9,35 @@ const getCategories = async () => {
     return response.rows;
 }
 
-module.exports = {  
+const getCategoryListingCountForCity = async (cityId) => {
+    const query = `SELECT categoryId, COUNT(*) as count FROM heidi_city_${cityId}.listings GROUP BY categoryId;`;
+    const response = await database.callQuery(query)
+    if (!response || !response.rows || response.rows.length === 0) {
+        return [];
+    }
+    return response.rows;
+}
+
+const getCategoryListingCount = async () => {
+    let query = `SELECT categoryId, COUNT(categoryId) AS totalCount FROM  (`;
+    let innerQuery = ``;
+    const cityConnection = await database.get(tables.CITIES_TABLE, null, "id");
+    for (const data of cityConnection.rows) {
+        innerQuery += `SELECT categoryId FROM heidi_city_${data.id}.listings UNION ALL `;
+    }
+    innerQuery = innerQuery.slice(0, -11);
+    query += innerQuery + `) AS combinedResults GROUP BY categoryId;`;
+
+    const response = await database.callQuery(query)
+    if (!response || !response.rows || response.rows.length === 0) {
+        return [];
+    }
+    return response.rows;
+}
+
+
+module.exports = {
     getCategories,
+    getCategoryListingCountForCity,
+    getCategoryListingCount,
 }
