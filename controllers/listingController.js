2ed2fbdbd9ca547ea0a098f9d345add73575902d
Aneesh Jose <aneesh@heidi-app.de>
2023-12-13 14:49:06 +0530

    refactor: use controller and services for getAllListings API

---
+++ controllers/listingController.js
@@ -0,0 +1,153 @@
+const supportedLanguages = require("../constants/supportedLanguages");
+const AppError = require("../utils/appError");
+const deepl = require("deepl-node");
+const { getCityListingsWithFiltersAndPagination } = require("../services/listingService");
+const { getCities } = require("../services/cities");
+const { getStatusById, getCategoryById, getSubCategoryById } = require("../services/cityListing");
+
+const getAllListings = async function (req, res, next) {
+    const params = req.query;
+    const pageNo = params.pageNo || 1;
+    const pageSize = params.pageSize || 9;
+    const filters = {};
+    let sortByStartDate = false;
+    let cities = [];
+
+    if (isNaN(Number(pageNo)) || Number(pageNo) <= 0) {
+        return next(
+            new AppError(`Please enter a positive integer for pageNo`, 400)
+        );
+    }
+    if (
+        isNaN(Number(pageSize)) ||
+        Number(pageSize) <= 0 ||
+        Number(pageSize) > 20
+    ) {
+        return next(
+            new AppError(
+                `Please enter a positive integer less than or equal to 20 for pageSize`,
+                400
+            )
+        );
+    }
+
+    if (params.sortByStartDate) {
+        const sortByStartDateString = params.sortByStartDate.toString()
+        if (sortByStartDateString !== 'true' && sortByStartDateString !== 'false') {
+            return next(
+                new AppError(`The parameter sortByCreatedDate can only be a boolean`, 400)
+            );
+        } else {
+            sortByStartDate = sortByStartDateString === 'true';
+        }
+    }
+
+    if (params.statusId) {
+        try {
+            const response = await getStatusById(params.statusId);
+            if (response && response.length === 0) {
+                return next(
+                    new AppError(`Invalid Status '${params.statusId}' given`, 400)
+                );
+            }
+        } catch (err) {
+            return next(new AppError(err));
+        }
+        filters.statusId = params.statusId;
+    }
+
+    if (params.categoryId) {
+        try {
+            const data = await getCategoryById(params.categoryId);
+            if (!data) {
+                return next(
+                    new AppError(`Invalid Category '${params.categoryId}' given`, 400)
+                );
+            } else {
+                if (params.subcategoryId) {
+                    try {
+                        const data = await getSubCategoryById(params.subcategoryId);
+                        if (!data) {
+                            return next(
+                                new AppError(
+                                    `Invalid subCategory '${params.subcategoryId}' given`,
+                                    400
+                                )
+                            );
+                        }
+                    } catch (err) {
+                        return next(new AppError(err));
+                    }
+                    filters.subcategoryId = params.subcategoryId;
+                }
+            }
+        } catch (err) {
+            return next(new AppError(err));
+        }
+        filters.categoryId = params.categoryId;
+    }
+
+    try {
+        if (params.cityId) {
+            const response = await getCities({ id: params.cityId });
+            if (response.rows.length === 0) {
+                return next(
+                    new AppError(`Invalid CityId '${params.cityId}' given`, 400)
+                );
+            }
+        } else {
+            cities = await getCities();
+
+        }
+    } catch (err) {
+        return next(new AppError(err));
+    }
+
+    try {
+        const listings = await getCityListingsWithFiltersAndPagination(params, pageNo, pageSize, cities, sortByStartDate)
+        const noOfListings = listings.length;
+        if (
+            noOfListings > 0 &&
+            params.translate &&
+            supportedLanguages.includes(params.translate)
+        ) {
+            const textToTranslate = [];
+            listings.forEach((listing) => {
+                textToTranslate.push(listing.title);
+                textToTranslate.push(listing.description);
+            });
+            const translator = new deepl.Translator(process.env.DEEPL_AUTH_KEY);
+            const translations = await translator.translateText(
+                textToTranslate,
+                null,
+                params.translate
+            );
+            for (let i = 0; i < noOfListings; i++) {
+                if (
+                    translations[2 * i].detectedSourceLang !== params.translate.slice(0, 2)
+                ) {
+                    listings[i].titleLanguage = translations[2 * i].detectedSourceLang;
+                    listings[i].titleTranslation = translations[2 * i].text;
+                }
+                if (
+                    translations[2 * i + 1].detectedSourceLang !==
+                    params.translate.slice(0, 2)
+                ) {
+                    listings[i].descriptionLanguage =
+                        translations[2 * i + 1].detectedSourceLang;
+                    listings[i].descriptionTranslation = translations[2 * i + 1].text;
+                }
+            }
+        }
+        return res.status(200).json({
+            status: "success",
+            data: listings,
+        });
+    } catch (err) {
+        return next(new AppError(err));
+    }
+};
+
+module.exports = {
+    getAllListings,
+};
--- services/listingService.js
+++ services/listingService.js
@@ -40,10 +40,43 @@ const getAllListingsWithFilters = async function (filters, cityId, pageNo, pageS
     return response.rows;
 }
 
+const getCityListingsWithFiltersAndPagination = async function (filters, pageNo, pageSize, cities, sortByStartDate) {
+    const individualQueries = [];
+    for (const city of cities) {
+        // if the city database is present in the city's server, then we create a federated table in the format
+        // heidi_city_{id}_listings and heidi_city_{id}_users in the core databse which points to the listings and users table respectively
+        let query = `SELECT L.*, U.username, U.firstname, U.lastname, U.image, U.id as coreUserId, ${city.id} 
+        as cityId FROM heidi_city_${city.id}${city.inCityServer ? "_" : "."}listings L 
+        inner join
+        user_cityuser_mapping UM on UM.cityUserId = L.userId AND UM.cityId = ${city.id}
+        inner join users U on U.id = UM.userId `;
+        if (filters.categoryId || filters.statusId) {
+            query += " WHERE ";
+            if (filters.categoryId) {
+                query += `L.categoryId = ${filters.categoryId} AND `;
+            }
+            if (filters.subcategoryId) {
+                query += `L.subcategoryId = ${filters.subcategoryId} AND `;
+            }
+            if (filters.statusId) {
+                query += `L.statusId = ${filters.statusId} AND `;
+            }
+            query = query.slice(0, -4);
+        }
+        individualQueries.push(query);
+    }
+
+    const query = `select * from (
+            ${individualQueries.join(" union all ")}
+        ) a order by ${sortByStartDate ? "startDate, createdAt" : "createdAt desc"} LIMIT ${(pageNo - 1) * pageSize}, ${pageSize};`;
+    const response = await database.callQuery(query);
+    return response.rows;
+}
 
 module.exports = {
     createListingWithTransaction,
     createUserListingMappingWithTransaction,
     getCityListingWithId,
     getAllListingsWithFilters,
+    getCityListingsWithFiltersAndPagination,
 }
