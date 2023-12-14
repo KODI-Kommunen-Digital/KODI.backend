ae8f08516f37357f527af6a3a075877e948b04bc
Aneesh Jose <aneesh@heidi-app.de>
2023-12-14 21:19:52 +0530

    refactor: getVillages for city API

---
+++ controllers/villageController.js
@@ -0,0 +1,24 @@
+const { getVillageForCity } = require("../services/villageService");
+const AppError = require("../utils/appError");
+
+const getVillages = async function (req, res, next) {
+    const cityId = req.cityId;
+
+    if (!cityId || isNaN(cityId)) {
+        return next(new AppError(`invalid cityId given`, 400));
+    }
+
+    try {
+        const villages = await getVillageForCity(cityId);
+        res.status(200).json({
+            status: "success",
+            data: villages,
+        });
+    } catch (err) {
+        return next(new AppError(err));
+    }
+}
+
+module.exports = {
+    getVillages,
+}
--- routes/village.js
+++ routes/village.js
@@ -1,28 +1,7 @@
 const express = require("express");
+const { getVillages } = require("../controllers/villageController");
 const router = express.Router();
-const database = require("../services/database");
-const tables = require("../constants/tableNames");
-const AppError = require("../utils/appError");
 
-router.get("/", async function (req, res, next) {
-    const cityId = req.cityId;
-
-    if (!cityId || isNaN(cityId)){
-        return next(new AppError(`invalid cityId given`, 400));
-    }
-    
-    database
-        .get(tables.VILLAGE_TABLE, null, null, cityId)
-        .then((response) => {
-            const data = response.rows;
-            res.status(200).json({
-                status: "success",
-                data,
-            });
-        })
-        .catch((err) => {
-            return next(new AppError(err));
-        });
-});
+router.get("/", getVillages);
 
 module.exports = router;
