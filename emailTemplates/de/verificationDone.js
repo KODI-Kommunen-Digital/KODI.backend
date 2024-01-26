fbfd231f8cef1b72efc3bf0cc131b36501e1abca
AjUm-HEIDI <ajay@heidi-app.de>
2024-01-26 13:49:50 +0100

    Update email subject and forgotpassword links

--- emailTemplates/de/verificationDone.js
+++ emailTemplates/de/verificationDone.js
@@ -1,6 +1,6 @@
 module.exports = function (firstName, lastName) {
     return {
-        Subject: "Dein Account wurde erfolgreich verifiziert",
+        subject: "Dein Account wurde erfolgreich verifiziert",
         body: `<h1> Dein Account wurde erfolgreich verifiziert",</h1>
                 <p>Hey ${firstName} ${lastName},<br>
                 Dein Account wurde erfolgreich verifiziert.<br>
--- emailTemplates/de/verifyEmail.js
+++ emailTemplates/de/verifyEmail.js
@@ -4,7 +4,7 @@ module.exports = function (firstName, lastName, token, userId, lang) {
         body: `<h1>Überprüfe deine E-Mail</h1>
                 <p>Hey ${firstName} ${lastName},<br>
                 Vielen Dank für die Registrierung. Um fortzufahren, musst du deine E-Mail verifizieren. Bitte klicke auf den Link, um mit der Verifizierung fortzufahren<br>
-                <a href="${process.env.WEBSITE_DOMAIN}/VerifyEmail?token=${token}&userId=${userId}&lang=${lang}">E-Mail-Link verifizieren</a>
+                <a href="${process.env.WEBSITE_DOMAIN}/VerifyEmail?token=${token}&userId=${userId}&lang=${lang}" target="_blank" rel="noopener noreferrer">E-Mail-Link verifizieren</a>
                 <br>
                 Viel Spaß! ,<br>
                 Das ${process.env.REGION}-Team</p>`
