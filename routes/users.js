94555eb2b687521fef46efcf417edee2238fa632
SoSa-HEIDI <sonu@heidi-app.de>
2024-04-02 15:48:33 +0200

    fixed sql injection issue for get /listings/search

--- routes/users.js
+++ routes/users.js
@@ -746,7 +746,6 @@ router.get("/:id/listings", async function (req, res, next) {
     if (req.query.statusId) {
         const statusId = req.query.statusId;
         
-        // check status id is valid or not before passing it into the query
         if (isNaN(Number(statusId)) || Number(statusId) <= 0) {
             next(new AppError(`Invalid status ${statusId}`, 400));
             return;
@@ -776,7 +775,6 @@ router.get("/:id/listings", async function (req, res, next) {
     if (req.query.categoryId) {
 
         const categoryId = req.query.categoryId;
-        // check category id is valid or not before passing it into the query
         if (isNaN(Number(categoryId)) || Number(categoryId) <= 0) {
             next(new AppError(`Invalid category ${categoryId}`, 400));
             return;
