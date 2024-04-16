f3b6730bc8601fbdb98313944ca481b1a2aeb770
AjUm-HEIDI <ajay@heidi-app.de>
2024-04-16 19:35:24 +0530

    Update ads endpoint to show ad once per day

--- routes/ads.js
+++ routes/ads.js
@@ -6,6 +6,9 @@ const AppError = require("../utils/appError");
 
 router.get("/", async function (req, res, next) {
     try {
+        const currentDate = new Date();
+        currentDate.setHours(0, 0, 0, 0);
+        
         const filter = {};
         if (req.query.cityId) {
             if(Number(req.query.cityId)) {
@@ -17,15 +20,18 @@ router.get("/", async function (req, res, next) {
         let data
         
         if(filter.cityId && Number(filter.cityId) && !isNaN(Number(filter.cityId))){
-            const query = `SELECT id, cityId, image, link, createdAt FROM ${tables.ADVERTISEMENTS} WHERE (cityId IS NULL OR cityId = ?) AND enabled = True`
-            const response = await database.callQuery(query, [Number(filter.cityId)])
+            const query = `SELECT id, cityId, image, link, createdAt FROM ${tables.ADVERTISEMENTS} WHERE (cityId IS NULL OR cityId = ?) AND enabled = True AND (lastShown IS NULL OR lastShown < ?)`
+            const response = await database.callQuery(query, [Number(filter.cityId), currentDate])
             data = response.rows
         } else {
-            const query = `SELECT id, cityId, image, link, createdAt FROM ${tables.ADVERTISEMENTS} WHERE cityId IS NULL AND enabled = True`
-            const response = await database.callQuery(query)
+            const query = `SELECT id, cityId, image, link, createdAt FROM ${tables.ADVERTISEMENTS} WHERE cityId IS NULL AND enabled = True AND (lastShown IS NULL OR lastShown < ?)`
+            const response = await database.callQuery(query, [currentDate])
             data = response.rows
         }
+
         const dataReturn = data[(Math.floor(Math.random() * data.length))]
+
+        await database.update(tables.ADVERTISEMENTS, { lastShown: currentDate}, { id: dataReturn.id })
         res.status(200).json({
             status: "success",
             data: dataReturn,
