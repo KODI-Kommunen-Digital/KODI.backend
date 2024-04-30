53dae326c3fb8e9305a518b2163e2b92251c9083
Moiz-HEIDI <130048960+Moiz-HEIDI@users.noreply.github.com>
2024-04-30 11:30:02 +0200

    updated the query

--- routes/ads.js
+++ routes/ads.js
@@ -28,59 +28,22 @@ router.get("/", async function (req, res, next) {
         } else {
             return next(new AppError("CityID is not given"));
         }
-        let listing
-
-        if (req.query.listingId) {
-            if(Number(req.query.listingId)) {
-                const response = await database.get(tables.LISTINGS_TABLE, {
-                    id: req.query.listingId,
-                }, null, req.query.cityId);
-                if (response.rows && response.rows.length === 0) {
-                    return next(new AppError(`Invalid Listing '${req.query.listingId}' given`, 400));
-                }
-                listing = response.rows[0];
-                if((listing.categoryId !== 1 && listing.categoryId !== 3) || listing.description.length < 650 || listing.createdAt < (currentDate - (12 * 60 * 60 * 1000)) ) {
-                    return res.status(200).json({
-                        status: "success",
-                    });
-                }
-
-            } else {
-                return next(new AppError("Invalid CityID given"));
-            }
-        } else {
-            return next(new AppError("ListingId is not given"));
-        }
-
-        if(listing) {
-            const query = `SELECT id, createdAt FROM listings WHERE createdAt > ? AND length(description) > 650 ORDER BY createdAt`
-            const response = await database.callQuery(query, [getDateInFormate(new Date(currentDate - (12 * 60 * 60 * 1000)))], Number(filter.cityId))
-            const data = response.rows
-            if(data[0].id !== Number(req.query.listingId)) {
-                return res.status(200).json({
-                    status: "success",
-                });
-            }
-        }
-        let data
         
-        if(filter.cityId && Number(filter.cityId) && !isNaN(Number(filter.cityId))){
-            const query = `SELECT id, cityId, image, link, createdAt, lastShown FROM ${tables.ADVERTISEMENTS} WHERE (cityId IS NULL OR cityId = ?) AND enabled = True`
-            const response = await database.callQuery(query, [Number(filter.cityId)])
-            data = response.rows
-        } else {
-            const query = `SELECT id, cityId, image, link, createdAt, lastShown FROM ${tables.ADVERTISEMENTS} WHERE cityId IS NULL AND enabled = True`
-            const response = await database.callQuery(query)
-            data = response.rows
+        const queryLisitngs = `SELECT id, createdAt FROM listings WHERE createdAt > ? AND length(description) > 650 AND categoryId IN (1,3) ORDER BY createdAt`
+        const responseListings = await database.callQuery(queryLisitngs, [getDateInFormate(new Date(currentDate - (12 * 60 * 60 * 1000)))], Number(filter.cityId))
+        const dataListnigs = responseListings.rows
+        if(!dataListnigs && dataListnigs.length <= 0 && dataListnigs[0].id !== Number(req.query.listingId)) {
+            return res.status(200).json({
+                status: "success",
+            });
         }
+        
+        
+        const query = `SELECT id, cityId, image, link, createdAt FROM ${tables.ADVERTISEMENTS} WHERE (cityId IS NULL OR cityId = ?) AND enabled = True`
+        const response = await database.callQuery(query, [Number(filter.cityId)])
+        const data = response.rows
 
         const dataReturn = data[(Math.floor(Math.random() * data.length))]
-
-        if(dataReturn) {
-            // const timeGap = listing.filter((list) => console.log(list.createdAt - dataReturn.lastShown))
-            console.log(listing)
-            console.log(dataReturn)
-        }
 
         if(dataReturn) {
             await database.update(tables.ADVERTISEMENTS, { lastShown: currentDate}, { id: dataReturn.id })
