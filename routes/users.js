16f9382e3fe766740f208082a60394ef87198e98
AjUm-HEIDI <ajay@heidi-app.de>
2024-05-08 13:47:38 +0200

    console log

--- routes/users.js
+++ routes/users.js
@@ -93,12 +93,13 @@ router.post("/login", async function (req, res, next) {
             userId: userData.id,
         });
         if (refreshData.rows.length > 0) {
-            console.log(refreshData)
-            console.log(sourceAddress, head.browsername, head.devicetype)
             const tokensToDelete = refreshData.rows.filter(token => 
                 token.sourceAddress === sourceAddress &&
                 token.browser === head.browsername &&
                 token.device === head.devicetype);
+            console.log("tokens to delete",tokensToDelete)
+            console.log("user details",sourceAddress, head.browsername, head.devicetype)
+
             if (tokensToDelete.length > 0) {
                 await database.deleteData(tables.REFRESH_TOKENS_TABLE, {
                     id: tokensToDelete.map(t => t.id),
