14831b0e80114be62afe86b1d668e71553e76062
Aneesh Jose <aneesh@heidi-app.com>
2024-11-20 05:00:25 +0530

    refactor: more info

--- v1/services/moreInfos.js
+++ v1/services/moreInfos.js
@@ -1,5 +1,6 @@
 const moreInfoTranslations = require("../constants/moreInfoTranslations");
-const moreInfoRepo = require("../repository/moreInfo");
+// const moreInfoRepo = require("../repository/moreInfo");
+const moreInfoRepository = require("../repository/moreInfoRepo");
 const AppError = require("../utils/appError");
 
 const getMoreInfo = async function (queryLanguage) {
@@ -8,7 +9,9 @@ const getMoreInfo = async function (queryLanguage) {
         language = "en";
     }
     try {
-        const data = await moreInfoRepo.getMoreInfoService();
+        // const data = await moreInfoRepo.getMoreInfoService();
+        const dataResp = await moreInfoRepository.getAll();
+        const data = dataResp.rows;
         data.forEach((d) => {
             d.title = moreInfoTranslations[language][d.title];
             d.isPdf = d.isPdf === 1;
