2ca8e376c7e92fd4104783f2a8e9481ea1ef1f69
Aneesh Jose <aneesh@heidi-app.com>
2024-11-20 04:34:12 +0530

    refactor: getVillages

--- v1/services/villages.js
+++ v1/services/villages.js
@@ -1,12 +1,14 @@
-const villageRepo = require("../repository/village");
 const AppError = require("../utils/appError");
+const villageRepository = require("../repository/villageRepo");
 
 const getVillages = async function (cityId) {
     if (!cityId || isNaN(cityId)) {
         throw new AppError(`invalid cityId given`, 400);
     }
     try {
-        return await villageRepo.getVillageForCity(cityId);
+        // return await villageRepo.getVillageForCity(cityId);
+        const villages = await villageRepository.getAll({ cityId });
+        return villages?.rows ?? [];
     } catch (err) {
         throw new AppError(err);
     }
