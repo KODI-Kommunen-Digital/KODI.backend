d44f52fb7d0bcb396f90c8b838b5056b4d07fdae
AjUm-HEIDI <ajay@heidi-app.de>
2024-12-17 16:13:25 +0100

    Add a folder for version v2

---
+++ v2/services/getUserListings.js
@@ -0,0 +1,127 @@
+const AppError = require("../utils/appError");
+const statusRepository = require("../repository/statusRepo");
+const categoriesRepository = require("../repository/categoriesRepo");
+const citiesRepository = require("../repository/citiesRepo");
+const listingsRepository = require("../repository/listingsRepo");
+
+// to refactor
+async function getUserListings(userId, statusId, categoryId, subcategoryId, pageNo, pageSize) {
+
+    const filters = {};
+
+    if (isNaN(Number(userId)) || Number(userId) <= 0) {
+        throw new AppError(`Invalid UserId ${userId}`, 400);
+    }
+    if (isNaN(Number(pageNo)) || Number(pageNo) <= 0) {
+        throw new AppError(`Please enter a positive integer for pageNo`, 400);
+    }
+    if (
+        isNaN(Number(pageSize)) ||
+        Number(pageSize) <= 0 ||
+        Number(pageSize) > 20
+    ) {
+        throw new AppError(
+            `Please enter a positive integer less than or equal to 20 for pageSize`,
+            400,
+        );
+    }
+
+    if (statusId) {
+        // check status id is valid or not before passing it into the query
+        if (isNaN(Number(statusId)) || Number(statusId) <= 0) {
+            throw new AppError(`Invalid status ${statusId}`, 400);
+        }
+
+        try {
+            const statuses = await statusRepository.getOne({
+                filters: [
+                    {
+                        key: "id",
+                        sign: "=",
+                        value: statusId,
+                    }
+                ],
+                columns: ["count(id) as statusCount"],
+            });
+            if (statuses.statusCount === 0) {
+                throw new AppError(`Invalid Status '${statusId}' given`, 400);
+            }
+        } catch (err) {
+            throw new AppError(err);
+        }
+        filters.statusId = statusId;
+    }
+
+    if (categoryId) {
+        // check category id is valid or not before passing it into the query
+        if (isNaN(Number(categoryId)) || Number(categoryId) <= 0) {
+            throw new AppError(`Invalid category ${categoryId}`, 400);
+        }
+
+        try {
+            const categories = await categoriesRepository.getAll({
+                filters: [
+                    {
+                        key: "id",
+                        sign: "=",
+                        value: categoryId,
+                    },
+                    {
+                        key: "isEnabled",
+                        sign: "=",
+                        value: true,
+                    }
+                ],
+            });
+            if (categories.count === 0) {
+                throw new AppError(`Invalid Category '${categoryId}' given`, 400);
+            } else {
+                if (subcategoryId) {
+                    // check subcategory id is valid or not before passing it into the query
+                    if (isNaN(Number(subcategoryId)) || Number(subcategoryId) <= 0) {
+                        throw new AppError(`Invalid subcategory ${subcategoryId}`, 400);
+                    }
+
+                    try {
+                        const subcategories = await categoriesRepository.getOne({
+                            filters: [
+                                {
+                                    key: "categoryId",
+                                    sign: "=",
+                                    value: categoryId,
+                                },
+                                {
+                                    key: "id",
+                                    sign: "=",
+                                    value: subcategoryId,
+                                }
+                            ],
+                            columns: ["count(id) as subcategoryCount"],
+                        });
+                        if (subcategories.subcategoryCount === 0) {
+                            throw new AppError(
+                                `Invalid subCategory '${subcategoryId}' given`,
+                                400,
+                            );
+                        }
+                    } catch (err) {
+                        throw new AppError(err);
+                    }
+                    filters.subcategoryId = subcategoryId;
+                }
+            }
+        } catch (err) {
+            throw new AppError(err);
+        }
+        filters.categoryId = categoryId;
+    }
+
+    try {
+        const cityMappings = await citiesRepository.getUserCityMapping(userId);
+        return await listingsRepository.retrieveCityListingsWithFilters(cityMappings, filters, pageNo, pageSize);
+    } catch (err) {
+        throw new AppError(err);
+    }
+}
+
+module.exports = { getUserListings };
