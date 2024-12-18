51d76a229a0d85f9e397d53851cfa76bc940a7e0
AjUm-HEIDI <ajay@heidi-app.de>
2024-12-18 14:07:12 +0100

    Update mysql

--- v2/utils/mysql.js
+++ v2/utils/mysql.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-function getCorePool() {
-    return mysql.createPool({
+async function getConnection() {
+    const pool = mysql.createPool({
         connectionLimit: process.env.DATABASE_POOL_MAX || 10, // default 10
         host: process.env.DATABASE_HOST,
         user: process.env.DATABASE_USER,
@@ -10,35 +10,7 @@ function getCorePool() {
         database: process.env.DATABASE_NAME,
         port: process.env.DATABASE_PORT || 3306,
     });
-}
-const pool = { 0: getCorePool() };
-
-async function getConnection(cityId) {
-    if (!cityId && !pool[0]) {
-        pool[0] = getCorePool();
-    }
-    if (pool[cityId || 0]) {
-        const connection = await pool[cityId || 0].getConnection();
-        return connection;
-    }
-    const coreConnection = await pool[0].getConnection();
-    const [rows] = await coreConnection.query(
-        `SELECT * FROM cities WHERE id = ?;`,
-        [cityId],
-    );
-    coreConnection.release();
-    const cityConnectionString = rows[0].connectionString;
-    const cityConnectionConfig = {};
-    cityConnectionString.split(";").forEach((element) => {
-        const elementList = element.split("=");
-        cityConnectionConfig[elementList[0]] = elementList[1];
-    });
-    cityConnectionConfig.host = cityConnectionConfig.server;
-    cityConnectionConfig.connectionLimit = process.env.DATABASE_POOL_MAX || 10;
-    delete cityConnectionConfig.server;
-    const cityConnection = mysql.createPool(cityConnectionConfig);
-    pool[cityId] = cityConnection;
-    const connection = cityConnection.getConnection();
+    const connection = await pool.getConnection();
     return connection;
 }
 
