6e0e4dc97936847bcc06e00055c57ad0aa10409f
AjUm-HEIDI <ajay@heidi-app.de>
2024-12-30 11:14:57 +0100

    update docs

--- docs/docRoot.js
+++ docs/docRoot.js
@@ -18,6 +18,7 @@ const deleteFavoriteListingSwagger = require("./favorites/deleteFavoriteListings
 const getFavoriteListingsSwagger = require("./favorites/getAllFavoriteListingsForUser");
 const getFavoritesSwagger = require("./favorites/getAllFavoritesForUser");
 const getAllListingsSwagger = require("./listings/getAllListings");
+const getAllCityListingsSwagger = require("./cityListings/getAllCityListings");
 const getMoreInfoSwagger = require("./moreInfo/getMoreInfo");
 const getAllStatusesSwagger = require("./status/getAllStatuses");
 const deleteLoggedInDevicesSwagger = require("./users/deleteLoggedInDevices");
@@ -47,184 +48,171 @@ const getWasteTypesSwagger = require('./wasteCalender/getWasteTypesSwagger');
 const getPickupDatesSwagger = require('./wasteCalender/getPickupDatesSwagger');
 const createDefectReport = require('./defectReport/createDefectReport');
 const voteOnListingSwagger = require('./cityListings/voteOnListingSwagger')
-
-const apiVersions = require('../constants/apiVersions');
-
-const apiDocumentation = {
-    openapi: '3.0.1',
-    info: {
-        version: '1.3.0',
-        title: 'KODI REST API - Documentation',
-        description: `KODI - Kommunen Digital is a REST API that will be used to fetch data to and from the frontend. It provides digital citizen services and participation opportunities for cities, counties, and municipalities.`,
-    },
-    components: {
-        securitySchemes: {
-            bearerAuth: {
-                type: 'http',
-                scheme: 'bearer',
-                bearerFormat: 'JWT',
+const apiDocumentation = (selectedVersion = '') => {
+    return {
+        openapi: '3.0.1',
+        info: {
+            version: '1.3.0',
+            title: 'KODI REST API - Documentation',
+            description: `KODI - Kommunen Digital is a REST API that will be used to fetch data to and from the frontend. It provides digital citizen services and participation opportunities for cities, counties, and municipalities.`,
+        },
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
             },
         },
-    },
-    servers: [
-        {
-            url: process.env.BASE_URL,
-            description: 'Base Server',
-        },
-        // ...process.env.API_VERSIONS.split(',').map((version) => ({
-        //     url: `${process.env.BASE_URL}/${version}`,
-        //     description: `${version} Server`,
-        // })),
-        ...Object.keys(apiVersions).map((version) => ({
-            url: `${process.env.BASE_URL}/${version}`,
-            description: `${version} Server`,
-        })),
-
-    ],
-    tags: [
-        {
-            name: 'Users',
-        },
-        {
-            name: 'Cities',
-        },
-        {
-            name: 'City Listings',
-        },
-        {
-            name: 'Ads',
-        },
-    ],
-    paths: {
-        '/users': {
-            'get': getAllUsersSwagger
-        },
-        '/users/{id}': {
-            'get': getUserByIdDoc,
-            'patch': updateUserSwagger,
-            'delete': deleteUserByIdSwagger,
-        },
-        '/users/login': {
-            'post': loginUser
-        },
-        '/users/register': {
-            'post': registerUserSwagger,
-        },
-        '/users/{id}/refresh': {
-            'post': refreshTokenSchema,
-        },
-        '/users/forgotPassword': {
-            'post': forgotPasswordSwagger,
-        },
-        '/users/resetPassword': {
-            'post': resetPasswordSwagger,
-        },
-        '/users/sendVerificationEmail': {
-            'post': sendVerificaltionEmailSwagger,
-        },
-        '/users/verifyEmail': {
-            'post': verifyEmailSwagger,
-        },
-        '/users/{id}/listings': {
-            'get': getUserListings,
-        },
-        '/users/myListings': {
-            'get': getMyListingsSwagger,
-        },
-        '/users/{id}/loginDevices': {
-            'post': getLoginDevicesSwagger,
-            'delete': deleteLoggedInDevicesSwagger,
-        },
-        '/users/{id}/imageUpload': {
-            'post': imageUpoadSwagger,
-        },
-        '/users/{id}/imageDelete': {
-            'delete': deleteUserProfilePicSwagger,
-        },
-        '/users/{id}/logout': {
-            'post': logoutSwagger,
-        },
-        '/cities': {
-            'get': getAllCitiesSwagger,
-        },
-        '/listings': {
-            'post': createCityListingSwagger,
-            'get': getAllListingsSwagger,
-        },
-        '/listings/{id}': {
-            'get': getCityListingByIdSwagger,
-            'patch': updateCityListingSwagger,
-            'delete': deleteCityListingSchemaSwagger,
-        },
-        '/listings/{id}/imageUpload': {
-            'post': uploadImageSchemaSwagger,
-        },
-        '/listings/{id}/imageDelete': {
-            'delete': deleteImageSchemaSwagger,
-        },
-        '/listings/{id}/pdfUpload': {
-            'post': uploadPDFSwagger,
-        },
-        '/listings/{id}/pdfDelete': {
-            'delete': deletePDFSchemaSwagger,
-        },
-        '/listings/:id/vote': {
-            'post': voteOnListingSwagger,
-        },
-        '/listings/search': {
-            'get': getSearchListingsSwagger,
-        },
-        '/listings/': {
-            'post': createListingSwagger
-        },
-        '/categories': {
-            'get': getAllCategoriesSwagger,
-        },
-        '/categories/{id}/subcategories': {
-            'get': getSubCategoriesSwagger,
-        },
-        '/categories/listingsCount': {
-            'get': getCategoriesListingCountSwagger,
-        },
-        '/status': {
-            'get': getAllStatusesSwagger,
-        },
-        '/citizenServices': {
-            'get': getAllCitizenServicesSwagger,
-        },
-        '/citizenServices/citizenServiceData': {
-            'get': getCitizenServiceDataSwagger,
-        },
-        '/contactUs': {
-            'post': contactUsSwagger,
-        },
-        '/moreinfo': {
-            'get': getMoreInfoSwagger,
-        },
-        '/users/{userId}/favorites': {
-            'get': getFavoritesSwagger,
-            'post': createFavoriteListingSwagger,
-        },
-        '/users/{userId}/favorites/listings': {
-            'get': getFavoriteListingsSwagger,
-        },
-        '/users/{userId}/favorites/{id}': {
-            'delete': deleteFavoriteListingSwagger,
-        },
-        '/cities/{cityId}/villages': {
-            'get': getVillegesSwagger,
-        },
-        '/ads/': {
-            'get': getAds,
-        },
-        ...(process.env.ENABLE_WASTE_CALENDER === 'True' && {
-            '/cities/:cityId/wasteCalender/streets': { 'get': getStreetsSwagger },
-            '/cities/:cityId/wasteCalender/wasteTypes': { 'get': getWasteTypesSwagger },
-            '/cities/:cityId/wasteCalender/streets/:streetId/pickupDates': { 'get': getPickupDatesSwagger },
-        }),
-        ...(process.env.ENABLE_DEFECT_REPORT === 'True' && {
-            '/reportDefect': { 'post': createDefectReport },
-        }),
+        servers: [
+            {
+                url: [process.env.BASE_URL, selectedVersion].join('/'),
+                description: `${selectedVersion || 'Base'} Server`,
+            },
+        ],
+        tags: [
+            {
+                name: 'Users',
+            },
+            {
+                name: 'Cities',
+            },
+            {
+                name: 'City Listings',
+            },
+            {
+                name: 'Ads',
+            },
+        ],
+        paths: {
+            '/users': {
+                'get': getAllUsersSwagger
+            },
+            '/users/{id}': {
+                'get': getUserByIdDoc,
+                'patch': updateUserSwagger,
+                'delete': deleteUserByIdSwagger,
+            },
+            '/users/login': {
+                'post': loginUser
+            },
+            '/users/register': {
+                'post': registerUserSwagger,
+            },
+            '/users/{id}/refresh': {
+                'post': refreshTokenSchema,
+            },
+            '/users/forgotPassword': {
+                'post': forgotPasswordSwagger,
+            },
+            '/users/resetPassword': {
+                'post': resetPasswordSwagger,
+            },
+            '/users/sendVerificationEmail': {
+                'post': sendVerificaltionEmailSwagger,
+            },
+            '/users/verifyEmail': {
+                'post': verifyEmailSwagger,
+            },
+            '/users/{id}/listings': {
+                'get': getUserListings,
+            },
+            '/users/myListings': {
+                'get': getMyListingsSwagger,
+            },
+            '/users/{id}/loginDevices': {
+                'post': getLoginDevicesSwagger,
+                'delete': deleteLoggedInDevicesSwagger,
+            },
+            '/users/{id}/imageUpload': {
+                'post': imageUpoadSwagger,
+            },
+            '/users/{id}/imageDelete': {
+                'delete': deleteUserProfilePicSwagger,
+            },
+            '/users/{id}/logout': {
+                'post': logoutSwagger,
+            },
+            '/cities': {
+                'get': getAllCitiesSwagger,
+            },
+            '/listings': {
+                'post': selectedVersion === 'v2' ? createCityListingSwagger : createListingSwagger,
+                'get': selectedVersion === 'v2' ? getAllCityListingsSwagger : getAllListingsSwagger
+            },
+            [selectedVersion === 'v2' ? '/listings/{id}' : '/cities/{cityId}/listings/{id}']: {
+                'get': getCityListingByIdSwagger,
+                'patch': updateCityListingSwagger,
+                'delete': deleteCityListingSchemaSwagger,
+            },
+            [selectedVersion === 'v2' ? '/listings/{id}/imageUpload' : '/cities/{cityId}/listings/{id}/imageUpload']: {
+                'post': uploadImageSchemaSwagger,
+            },
+            [selectedVersion === 'v2' ? '/listings/{id}/imageDelete' : '/cities/{cityId}/listings/{id}/imageDelete']: {
+                'delete': deleteImageSchemaSwagger,
+            },
+            [selectedVersion === 'v2' ? '/listings/{id}/pdfUpload' : '/cities/{cityId}/listings/{id}/pdfUpload']: {
+                'post': uploadPDFSwagger,
+            },
+            [selectedVersion === 'v2' ? '/listings/{id}/pdfDelete' : '/cities/{cityId}/listings/{id}/pdfDelete']: {
+                'delete': deletePDFSchemaSwagger,
+            },
+            [selectedVersion === 'v2' ? '/listings/{id}/vote' : '/cities/{cityId}/listings/{id}/vote']: {
+                'post': voteOnListingSwagger,
+            },
+            '/listings/search': {
+                'get': getSearchListingsSwagger,
+            },
+            '/categories': {
+                'get': getAllCategoriesSwagger,
+            },
+            '/categories/{id}/subcategories': {
+                'get': getSubCategoriesSwagger,
+            },
+            '/categories/listingsCount': {
+                'get': getCategoriesListingCountSwagger,
+            },
+            '/status': {
+                'get': getAllStatusesSwagger,
+            },
+            '/citizenServices': {
+                'get': getAllCitizenServicesSwagger,
+            },
+            '/citizenServices/citizenServiceData': {
+                'get': getCitizenServiceDataSwagger,
+            },
+            '/contactUs': {
+                'post': contactUsSwagger,
+            },
+            '/moreinfo': {
+                'get': getMoreInfoSwagger,
+            },
+            '/users/{userId}/favorites': {
+                'get': getFavoritesSwagger,
+                'post': createFavoriteListingSwagger,
+            },
+            '/users/{userId}/favorites/listings': {
+                'get': getFavoriteListingsSwagger,
+            },
+            '/users/{userId}/favorites/{id}': {
+                'delete': deleteFavoriteListingSwagger,
+            },
+            '/cities/{cityId}/villages': {
+                'get': getVillegesSwagger,
+            },
+            '/ads/': {
+                'get': getAds,
+            },
+            ...(process.env.ENABLE_WASTE_CALENDER === 'True' && {
+                '/cities/:cityId/wasteCalender/streets': { 'get': getStreetsSwagger },
+                '/cities/:cityId/wasteCalender/wasteTypes': { 'get': getWasteTypesSwagger },
+                '/cities/:cityId/wasteCalender/streets/:streetId/pickupDates': { 'get': getPickupDatesSwagger },
+            }),
+            ...(process.env.ENABLE_DEFECT_REPORT === 'True' && {
+                '/reportDefect': { 'post': createDefectReport },
+            }),
+        }
     }
 };
 
--- v2/repository/categoriesRepo.js
+++ v2/repository/categoriesRepo.js
@@ -8,17 +8,14 @@ class CategoriesRepo extends BaseRepo {
     }
 
     getCategoryListingCount = async (cityIds) => {
-        // Convert cityIds array to a comma-separated string to use in the SQL IN clause
-        // Also, handling the scenario where cityIds might be empty
-        const cityIdsString = cityIds.length > 0 ? cityIds.join(', ') : 'NULL';
 
         // SQL query that dynamically includes or excludes the city filter based on whether cityIds are provided
         const query = `
             SELECT categoryId, COUNT(categoryId) AS totalCount
             FROM listings
-            LEFT JOIN citylisting ON listings.id = city_listing_mappings.listingId
+            LEFT JOIN city_listing_mappings ON listings.id = city_listing_mappings.listingId
             WHERE listings.statusId = 1
-            AND (${cityIds.length > 0 ? `city_listing_mappings.cityId IN (${cityIdsString})` : 'TRUE'})
+            AND (${cityIds.length > 0 ? `city_listing_mappings.cityId IN (${cityIds.join(', ')})` : 'TRUE'})
             GROUP BY categoryId;
         `;
         const response = await database.callQuery(query);
