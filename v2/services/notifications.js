18bca146e0cfc300f7bd35d3a96b0d384a112719
SoSa-HEIDI <sonu@heidi-app.de>
2025-01-29 14:05:56 +0100

    updated the api to accept all cities/category

--- v2/services/notifications.js
+++ v2/services/notifications.js
@@ -2,6 +2,8 @@ const AppError = require("../utils/appError");
 const usersRepository = require("../repository/userRepo");
 const UserPreferenceCategoriesRepo = require("../repository/userPreferenceCategoriesRepo");
 const userPreferenceCitiesRepo = require("../repository/userPreferenceCitiesRepo");
+const cityRepository = require("../repository/citiesRepo");
+const categoryRepository = require("../repository/categoriesRepo");
 
 const updateAllNotifications = async function(userId, enabled){
     try {
@@ -101,42 +103,66 @@ const getUserNotificationPreference = async function(userId){
 const updateUserNotificationPreference = async function(userId, {type, id, enabled}){
     try {
         if(type === 'CITY_PREFERENCE'){
-            if (enabled){
-                await userPreferenceCitiesRepo.insertCityPreferenceUnique(userId, id);
-            } else {
-                await userPreferenceCitiesRepo.delete({
-                    filters: [
-                        {
-                            key: "userId",
-                            sign: "=",
-                            value: userId
-                        },
-                        {
-                            key: "cityId",
-                            sign: "=",
-                            value: id
-                        }
-                    ]
-                });
+            if (id){
+                if (enabled){
+                    await userPreferenceCitiesRepo.insertCityPreferenceUnique(userId, id);
+                } else {
+                    await userPreferenceCitiesRepo.delete({
+                        filters: [
+                            {
+                                key: "userId",
+                                sign: "=",
+                                value: userId
+                            },
+                            {
+                                key: "cityId",
+                                sign: "=",
+                                value: id
+                            }
+                        ]
+                    });
+                }
+            } else { // if no id, then delete all city preference of that user
+                if (enabled) {
+                    const allCities = await cityRepository.getAll(); 
+                    const data = allCities.rows.map(city => ({userId, cityId: city.id}));
+                    await userPreferenceCitiesRepo.insertMultipleCityPreference(data); // Bulk insert all cities for the user
+                } else {
+                    await userPreferenceCitiesRepo.delete({
+                        filters: [{ key: "userId", sign: "=", value: userId }]
+                    });
+                }
             }
         } else if (type === 'CATEGORY_PREFERENCE'){
-            if (enabled){
-                await UserPreferenceCategoriesRepo.insertCategoryPreferenceUnique(userId, id);
-            } else {
-                await userPreferenceCitiesRepo.delete({
-                    filters: [
-                        {
-                            key: "userId",
-                            sign: "=",
-                            value: userId
-                        },
-                        {
-                            key: "categoryId",
-                            sign: "=",
-                            value: id
-                        }
-                    ]
-                });
+            if(id) {
+                if (enabled){
+                    await UserPreferenceCategoriesRepo.insertCategoryPreferenceUnique(userId, id);
+                } else {
+                    await UserPreferenceCategoriesRepo.delete({
+                        filters: [
+                            {
+                                key: "userId",
+                                sign: "=",
+                                value: userId
+                            },
+                            {
+                                key: "categoryId",
+                                sign: "=",
+                                value: id
+                            }
+                        ]
+                    });
+                }
+            } else { // if no id, then delete all category preference of that user
+                if (enabled) {
+                    const allCategories = await categoryRepository.getAll(); 
+                    const insertData = allCategories.rows.map(category => ({userId, categoryId:category.id}));
+                    await UserPreferenceCategoriesRepo.insertMultipleCategoryPreference(insertData); // Bulk insert all categories for the user
+                } else {
+                    await UserPreferenceCategoriesRepo.delete({
+                        filters: [{ key: "userId", sign: "=", value: userId }]
+                    });
+                }
             }
         } else {
             throw new AppError('Invalid preference type', 400);
@@ -147,6 +173,5 @@ const updateUserNotificationPreference = async function(userId, {type, id, enabl
         throw new AppError(err);    
     }
 }
-
 
 module.exports = { updateAllNotifications, getUserNotificationPreference , updateUserNotificationPreference };
