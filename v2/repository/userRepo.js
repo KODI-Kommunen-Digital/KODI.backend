7e5baed4b667d05f3f4e108845bb5739167dda3c
SoSa-HEIDI <sonu@heidi-app.de>
2025-02-12 15:53:40 +0100

    push notification preference for admins as well

--- v2/repository/userRepo.js
+++ v2/repository/userRepo.js
@@ -26,9 +26,20 @@ class UserRepo extends BaseRepo {
         const query = `SELECT DISTINCT u.id AS userId FROM users u
             JOIN user_preference_cities upc ON u.id = upc.userId
             JOIN user_preference_categories upcat ON u.id = upcat.userId
-            WHERE upc.cityId = ? AND upcat.categoryId = ?;
+            WHERE upc.cityId IN (?) AND upcat.categoryId = ?;
         `;
         const response = await database.callQuery(query, [cityId, categoryId]);
+        return response.rows;
+    }
+
+    getUsersForNotificationWithUserFilter = async (cityId, categoryId, userIds) => {
+        const query = `SELECT DISTINCT u.id AS userId FROM users u
+            JOIN user_preference_cities upc ON u.id = upc.userId
+            JOIN user_preference_categories upcat ON u.id = upcat.userId
+            WHERE upc.cityId IN (?) 
+            AND upcat.categoryId = ? 
+            AND u.id IN (?);`;
+        const response = await database.callQuery(query, [cityId, categoryId, userIds]);
         return response.rows;
     }
 
