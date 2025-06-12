7c0bc58b688b34a2e404f456c8574b0986562d46
spector <naren@ff.com>
2025-06-12 13:10:24 +0000

    final push

--- v2/services/listings.js
+++ v2/services/listings.js
@@ -955,13 +955,13 @@ const handleUnifiedChat = async ({
     });
 
     try {
-        // if (process.env.WEBSOCKET_ENABLED) {
-        //     console.log('sending websocket request');
-        //     await axios.post(
-        //         `${process.env.WEBSOCKET_SERVER_ADDR}/publish/${websoketChannelId}?accessToken=${process.env.WEBSOCKET_ACCESS_TOKEN}`,
-        //         { type: 'newMessage', data: chatWithDetails }
-        //     );
-        // }
+        if (process.env.WEBSOCKET_ENABLED) {
+            console.log('sending websocket request');
+            await axios.post(
+                `${process.env.WEBSOCKET_SERVER_ADDR}/publish/${websoketChannelId}?accessToken=${process.env.WEBSOCKET_ACCESS_TOKEN}`,
+                { type: 'newMessage', data: chatWithDetails }
+            );
+        }
         const payload = {
             listingId: `${listingId}`,
             type: "listing_chat",
