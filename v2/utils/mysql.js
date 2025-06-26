580cb8c752e321e1f51b3444b1bfce87b59acd6c
spector <naren@ff.com>
2025-06-26 11:06:59 +0000

    testing hangup

--- v2/utils/mysql.js
+++ v2/utils/mysql.js
@@ -9,22 +9,28 @@ function getCorePool() {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         port: process.env.DATABASE_PORT || 3306,
-        timezone: 'local',
+        timezone: "local",
         typeCast: function (field, next) {
-            if (field.type === 'DATETIME' || field.type === 'TIMESTAMP') {
+            if (field.type === "DATETIME" || field.type === "TIMESTAMP") {
                 return field.string();
             }
             return next();
-        }
+        },
     });
 }
 const pool = { 0: getCorePool() };
+setInterval(() => {
+    const corePool = pool[0].pool;
+    if (corePool && corePool._allConnections) {
+        console.log(`[MySQL] Total: ${corePool._allConnections.length}, Free: ${corePool._freeConnections.length}, Queue:${corePool._connectionQueue.length}`);
+    }
+}, 5000)
 
 async function getConnection(cityId) {
     if (!pool[0]) {
         pool[0] = getCorePool();
     }
-    const coreConnection = await pool[0].getConnection()
+    const coreConnection = await pool[0].getConnection();
     if (!cityId) {
         return coreConnection;
     }
@@ -34,7 +40,7 @@ async function getConnection(cityId) {
     }
     const [rows] = await coreConnection.query(
         `SELECT * FROM cities WHERE id = ?;`,
-        [cityId],
+        [cityId]
     );
     coreConnection.release();
     const cityConnectionString = rows[0].connectionString;
