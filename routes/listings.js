162ea7e3e5155192a672069dfc4814ae610c57e2
Aneesh Jose <aneesh@heidi-app.com>
2025-08-01 11:34:20 +0530

    invalidd data

--- routes/listings.js
+++ routes/listings.js
@@ -284,13 +284,15 @@ router.get("/", async function (req, res, next) {
             }
         }
 
-        // Remove viewCount from listings
-        listings.forEach(listing => delete listing.viewCount);
-
         // Send response
         return res.status(200).json({
             status: "success",
-            data: listings,
+            data: listings.map(listing => ({
+                ...listing,
+                firstname: "****",
+                lastname: '****',
+                viewCount: undefined, // Exclude viewCount
+            })),
         });
     } catch (err) {
         return next(new AppError(err));
