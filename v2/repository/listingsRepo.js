03ca7f41bffabdf56b4928170bc4a8af1737e84d
ankit.heer <ankit.heer>
2025-08-08 10:43:15 +0530

    removed console log

--- v2/repository/listingsRepo.js
+++ v2/repository/listingsRepo.js
@@ -111,7 +111,6 @@ class ListingsRepo extends BaseRepo {
         queryParams.push(parseInt(offset, 10), parseInt(pageSize, 10));
 
         try {
-            console.log("Executing query:", paginationQuery, "with params:", queryParams);
             const response = await database.callQuery(paginationQuery, queryParams);
             return response.rows;
         } catch (error) {
