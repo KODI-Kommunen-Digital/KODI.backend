d38fecbb94e890ac8b9964845223005e896a8335
Saurabh Patel <dev7@heidi-app.com>
2025-09-30 19:40:42 +0530

    refactor getUsersForNotification to conditionally join user_preference_categories

--- v2/repository/userRepo.js
+++ v2/repository/userRepo.js
@@ -23,12 +23,19 @@ class UserRepo extends BaseRepo {
     }
 
     getUsersForNotification = async (cityId, categoryId) => {
-        const query = `SELECT DISTINCT u.id AS userId FROM users u
+        let query = `SELECT DISTINCT u.id AS userId FROM users u
             JOIN user_preference_cities upc ON u.id = upc.userId
-            JOIN user_preference_categories upcat ON u.id = upcat.userId
-            WHERE upc.cityId IN (?) AND upcat.categoryId = ?;
-        `;
-        const response = await database.callQuery(query, [cityId, categoryId]);
+            WHERE upc.cityId IN (?)`;
+        const params = [cityId];
+
+        if (categoryId !== undefined && categoryId !== null) {
+            query += `
+                JOIN user_preference_categories upcat ON u.id = upcat.userId
+                AND upcat.categoryId = ?`;
+            params.push(categoryId);
+        }
+
+        const response = await database.callQuery(query, params);
         return response.rows;
     }
 
@@ -36,8 +43,8 @@ class UserRepo extends BaseRepo {
         const query = `SELECT DISTINCT u.id AS userId FROM users u
             JOIN user_preference_cities upc ON u.id = upc.userId
             JOIN user_preference_categories upcat ON u.id = upcat.userId
-            WHERE upc.cityId IN (?) 
-            AND upcat.categoryId = ? 
+            WHERE upc.cityId IN (?)
+            AND upcat.categoryId = ?
             AND u.id IN (?);`;
         const response = await database.callQuery(query, [cityId, categoryId, userIds]);
         return response.rows;
