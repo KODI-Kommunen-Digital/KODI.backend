fd6e7c901d9e7648c8a5c4593078e493bd9094fc
Saurabh Patel <dev7@heidi-app.com>
2025-10-01 21:42:53 +0530

    feat: add listing scheduler cron service to manage scheduled listings notifications

---
+++ v2/services/listingSchedulerCron.js
@@ -0,0 +1,131 @@
+const cron = require("node-cron");
+const database = require("../utils/database");
+const sendPushNotification = require("./sendPushNotification");
+const status = require("../constants/status");
+require("dotenv").config();
+
+const cronSchedule = process.env.CRON_SCHEDULE || "*/5 * * * *";
+
+class ListingSchedulerCron {
+    constructor() {
+        this.cronJob = null;
+    }
+
+    start() {
+        this.cronJob = cron.schedule(cronSchedule, async () => {
+            try {
+                await this.checkScheduledListings();
+                console.log("Cron job executed successfully");
+            } catch (error) {
+                console.error("Error in notification cron job:", error);
+            }
+        });
+        console.log("Listing notification cron job started");
+    }
+
+    stop() {
+        if (this.cronJob) {
+            this.cronJob.stop();
+            console.log("Listing notification cron job stopped");
+        }
+    }
+
+    async checkScheduledListings() {
+        const now = new Date();
+        const formatDateForMySQL = (date) =>
+            date.toISOString().slice(0, 19).replace("T", " ");
+        const nowInGermany = new Date(
+            now.toLocaleString("en-US", { timeZone: "Europe/Berlin" })
+        );
+
+        const formattedNow = formatDateForMySQL(nowInGermany);
+
+        const query = `
+        SELECT
+            l.id,
+            l.title,
+            IFNULL(
+                JSON_ARRAYAGG(JSON_OBJECT('cityId', lcm.cityId, 'order', lcm.cityOrder)),
+                JSON_ARRAY()
+            ) AS cities
+            FROM listings l
+            LEFT JOIN city_listing_mappings lcm
+                ON l.id = lcm.listingId
+            WHERE l.scheduledAt <= '${formattedNow}'
+                AND l.statusId = ${status.Scheduled}
+            GROUP BY l.id, l.title;
+        `;
+
+        const data = await database.callQuery(query);
+        const rows = data.rows;
+
+        for (const listing of rows || []) {
+            try {
+                const listingId = listing.id;
+                const cityMappings = JSON.parse(listing.cities);
+                const cityIds = cityMappings.map((mapping) => mapping.cityId);
+
+                // Fetch main city from listing.cities with order 1
+                const mainCityMapping = cityMappings.find(
+                    (mapping) => mapping.order === 1
+                );
+                if (!mainCityMapping) {
+                    console.warn(
+                        `No main city found for listing ${listingId}, skipping notification.`
+                    );
+                    continue;
+                }
+                const mainCityId = mainCityMapping.cityId;
+
+                // Fetch main city details
+                const cityData = await database.callQuery(
+                    "SELECT id, name FROM cities WHERE id = ?",
+                    [mainCityId]
+                );
+                if (cityData.rows.length === 0) {
+                    console.warn(
+                        `Main city with ID ${mainCityId} not found for listing ${listingId}, skipping notification.`
+                    );
+                    continue;
+                }
+                const mainCity = cityData.rows[0];
+
+                await sendPushNotification.sendPushNotificationsToUsers(
+                    cityIds,
+                    null,
+                    "Eilmeldung",
+                    mainCity.name + " - " + listing.title,
+                    { cityId: mainCity.id.toString(), id: listingId.toString() }
+                );
+
+                // Update listing status to Active
+                await database.callQuery(
+                    "UPDATE listings SET statusId = 1 WHERE id = ?",
+                    [listingId]
+                );
+
+                console.log(`Sent notification for listing ${listing.id}`);
+            } catch (error) {
+                console.error(`Error processing listing ${listing.id}:`, error);
+            }
+        }
+
+        console.log("Checked scheduled listings at", formattedNow);
+        console.log("Listings processed:", rows.length);
+    }
+}
+
+const listingSchedulerCron = new ListingSchedulerCron();
+
+// Handle process termination
+process.on("SIGINT", () => {
+    listingSchedulerCron.stop();
+    process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+    listingSchedulerCron.stop();
+    process.exit(0);
+});
+
+module.exports = listingSchedulerCron;
