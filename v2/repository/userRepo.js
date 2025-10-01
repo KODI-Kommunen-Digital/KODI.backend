282d4dfbafbac2ebca9c97f24673a884f5d5604e
Saurabh Patel <dev7@heidi-app.com>
2025-10-01 16:05:05 +0530

    refactor getUsersForNotification to enhance query structure and improve readability

--- v2/repository/userRepo.js
+++ v2/repository/userRepo.js
@@ -24,14 +24,22 @@ class UserRepo extends BaseRepo {
 
     getUsersForNotification = async (cityId, categoryId) => {
         let query = `SELECT DISTINCT u.id AS userId FROM users u
-            JOIN user_preference_cities upc ON u.id = upc.userId
-            WHERE upc.cityId IN (?)`;
+            JOIN user_preference_cities upc ON u.id = upc.userId`;
+
         const params = [cityId];
 
         if (categoryId !== undefined && categoryId !== null) {
             query += `
-                JOIN user_preference_categories upcat ON u.id = upcat.userId
-                AND upcat.categoryId = ?`;
+            JOIN user_preference_categories upcat ON u.id = upcat.userId`;
+        }
+
+        query += `
+            WHERE upc.cityId IN (?)
+            AND u.allNotificationsEnabled = 1`;
+
+        if (categoryId !== undefined && categoryId !== null) {
+            query += `
+            AND upcat.categoryId = ?`;
             params.push(categoryId);
         }
 
@@ -45,7 +53,8 @@ class UserRepo extends BaseRepo {
             JOIN user_preference_categories upcat ON u.id = upcat.userId
             WHERE upc.cityId IN (?)
             AND upcat.categoryId = ?
-            AND u.id IN (?);`;
+            AND u.id IN (?)
+            AND u.allNotificationsEnabled = 1;`;
         const response = await database.callQuery(query, [cityId, categoryId, userIds]);
         return response.rows;
     }
