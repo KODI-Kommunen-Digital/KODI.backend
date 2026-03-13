bc9a578115ab7e69acf7a026e49b482b4d1972d3
bhautik.multiqos <dev25@heidi-app.com>
2026-03-13 19:25:58 +0530

    city id changes done.

--- routes/cityListings.js
+++ routes/cityListings.js
@@ -753,7 +753,7 @@ router.delete("/:id", authentication, async function (req, res, next) {
 
         const prefix = `user_${req.userId}/city_${cityId}_listing_${id}`;
 
-        const {rows: listingImages} = await database.callQuery(query, [prefix]);
+        const {rows: listingImages} = await database.callQuery(query, [prefix], cityId);
         const userImageList = listingImages.map(img => ({
             Key: img.logo
         }));
@@ -1262,7 +1262,7 @@ router.delete(
 
         const prefix = `user_${req.userId}/city_${cityId}_listing_${id}`;
 
-        const {rows: listingImages} = await database.callQuery(query, [prefix]);
+        const {rows: listingImages} = await database.callQuery(query, [prefix], cityId);
         const userImageList = listingImages.map(img => ({
             Key: img.logo
         }));
