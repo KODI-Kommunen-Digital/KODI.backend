fe2f127e7229cbdee24985978c00fb2a28665e90
bhautik.multiqos <dev25@heidi-app.com>
2026-03-13 17:43:11 +0530

    - cityId changes done.

--- v1/services/cityListings.js
+++ v1/services/cityListings.js
@@ -1282,7 +1282,7 @@ const deleteImageForCityListing = async function (id, cityId, userId, roleId) {
 
     const prefix = `user_${userId}/city_${cityId}_listing_${id}`;
 
-    const {rows: listingImages} = await database.callQuery(query, [prefix]);
+    const {rows: listingImages} = await database.callQuery(query, [prefix], cityId);
     const userImageList = listingImages.map(img => ({
         Key: img.logo
     }));
