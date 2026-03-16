9e53cc71a721d5f8af1e2fb1e6f622a25424ac29
bhautik.multiqos <dev25@heidi-app.com>
2026-03-16 14:28:56 +0530

    - city Listing all images logic added for the delete listing api.

--- routes/cityListings.js
+++ routes/cityListings.js
@@ -751,7 +751,7 @@ router.delete("/:id", authentication, async function (req, res, next) {
             WHERE logo LIKE ?
         `;
 
-        const prefix = `user_${req.userId}/city_${cityId}_listing_${id}`;
+        const prefix = `user_${req.userId}/city_${cityId}_listing_${id}%`;
 
         const {rows: listingImages} = await database.callQuery(query, [prefix], cityId);
         const userImageList = listingImages.map(img => ({
